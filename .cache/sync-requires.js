01bfb0dd93bd9c702bb3c381ffb3a98a07455ce1
ME ADMIN <ME@MEs-MacBook-Pro.local>
2020-06-16 20:57:11 +0200

    adding master update

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/src/pages/page-2.js")))
+}
+
