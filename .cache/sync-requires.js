be08fb3a14681508cd6cc619216c0abe2596a6ef
ME ADMIN <ME@MEs-MacBook-Pro.local>
2020-06-16 21:17:22 +0200

    updating master branch

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/Malick-mbengueWebsite/src/pages/page-2.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/malick/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/malick/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/BackUps/2gatsby-starter-dimension copy 3/malick/src/pages/page-2.js")))
 }
 
